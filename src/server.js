3751e030dd0302a8d95d734352ace4fb6568b8df
hankug <hankug1@naver.com>
2022-05-27 08:29:36 +0900

    socket chat test

--- src/server.js
+++ src/server.js
@@ -16,9 +16,15 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-function handlerConnection(socket) {
-    console.log(socket);
-}
-wss.on("connection", handlerConnection);
+const sockets = [];
+
+wss.on("connection", (socket) => {
+    sockets.push(socket);
+    console.log("Connected to Browser ✅");
+    socket.on("close", () => console.log("Disconnected from Client ❌"));
+    socket.on("message", (message) => {
+        sockets.forEach(aSocket => aSocket.send(message.toString("utf-8")))
+    });
+});
 
 server.listen(3000, handleListen);
