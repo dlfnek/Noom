8cef5bf8e30e91752fb38e1c920386a5caf7b908
hankug <hankug1@naver.com>
2022-06-02 00:04:45 +0900

    make chatting

--- src/server.js
+++ src/server.js
@@ -20,11 +20,23 @@ const wsServer = SocketIO(httpServer);
 const sockets = [];
 
 wsServer.on("connection", (socket) => {
+    socket.onAny((event) => {
+        console.log(`Socket Event: ${event}`);
+    });
+
     socket.on("enter_room", (roomName, done) => {
-        console.log(roomName);
-        setTimeout(() => {
-            done("hello from the backend");
-        }, 15000);
+        socket.join(roomName);
+        done();
+        socket.to(roomName).emit("welcome");
+    });
+
+    socket.on("disconnecting", () => {
+        socket.rooms.forEach((room) => socket.to(room).emit("bye"));
+    });
+
+    socket.on("new_message", (msg, room, done) => {
+        socket.to(room).emit("new_message", msg);
+        done();
     });
 });
 // wss.on("connection", (socket) => {
